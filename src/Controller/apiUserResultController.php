2935a72ea0b637e82e5f95ce630a7245a776f630
Jose Lorenzo <jolorenzom@gmail.com>
2018-12-23 14:34:34 +0100

    [User-Result] GET /users/{idUser}/results GET /users/{idUser}/results/{idResults}

---
+++ src/Controller/apiUserResultController.php
@@ -0,0 +1,232 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jolo
+ * Date: 23/12/18
+ * Time: 13:46
+ */
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Result;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class apiUserResultController
+ *
+ * @package App\Controller
+ *
+ * @Route(path=apiUserResultController::API_USER_RESULT, name="api_user_result_")
+ */
+class apiUserResultController extends AbstractController
+{
+    public const API_USER_RESULT = '/api/v1/users';
+
+    /**
+     * @param User $user
+     * @Route(path="/{id}/results", name="getAll", methods={ Request::METHOD_GET })
+     * @return JsonResponse
+     */
+    public function getAllResultsOfUser(?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return $this->error(Response::HTTP_NOT_FOUND, 'USER DOESNT EXISTS IN DB');
+        }
+
+        /** @var Result[] results */
+        $results = $this->getDoctrine()
+            ->getRepository(Result::class)
+            ->findBy(['user'=>$user->getId()]);
+
+        return (null === $results)
+            ? $this->error(Response::HTTP_NOT_FOUND, 'NOT FOUND')
+            : new JsonResponse($results);
+    }
+
+    /**
+     * @Route(path="/{user}/results/{result}", name="getOne", methods={ Request::METHOD_GET })
+     * @param Result|null $result
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function getOneResultOfUser(?User $user, ?Result $result): JsonResponse
+    {
+        if (null === $user) {
+            return $this->error(Response::HTTP_NOT_FOUND, 'USER DOESNT EXISTS IN DB');
+        }
+
+        if (null === $result) {
+            return $this->error(Response::HTTP_NOT_FOUND, 'RESULT DOESNT EXISTS IN DB');
+        }
+
+        /** @var Result resultDB */
+        $resultsDB = $this->getDoctrine()
+            ->getRepository(Result::class)
+            ->findOneBy([
+                'id'=>$result->getId(),
+                'user'=>$user->getId()
+            ]);
+
+        return (null === $resultsDB)
+            ? $this->error(Response::HTTP_NOT_FOUND, 'NOT FOUND')
+            : new JsonResponse($resultsDB);
+    }
+
+    /**
+     * @param Request $request
+     * @Route(path="", name="post", methods={ Request::METHOD_POST })
+     * @return JsonResponse
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function postResult(Request $request): JsonResponse
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $datosPeticion = $request->getContent();
+        $datos = json_decode($datosPeticion, true);
+        $userId = $datos['user'] ?? null;
+        $newResult = $datos['result'] ?? null;
+        $newTimestamp = new \DateTime('now');
+
+        // Error: falta USER
+        if (null === $userId) {
+            return $this->error(Response::HTTP_UNPROCESSABLE_ENTITY, 'Falta USER');
+        }
+
+        // Error: falta RESULT
+        if (null === $newResult) {
+            return $this->error(Response::HTTP_UNPROCESSABLE_ENTITY, 'Falta RESULT');
+        }
+
+        //Error: USER no existe
+        $userDB = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find($userId);
+        if (null === $userDB) {
+            return $this->error(Response::HTTP_NOT_FOUND, 'USER NOT FOUND');
+        }
+
+        // Crear Result
+        $result = new Result($newResult,$newTimestamp,$userDB);
+
+        // Hacer persistente RESULT
+        $em->persist($result);
+        $em->flush();
+
+        // devolver respuesta
+        return new JsonResponse($result, Response::HTTP_CREATED);
+    }
+
+    /**
+     * @param Request $request
+     * @param Result $result
+     * @Route(path="/{id}", name="put", methods={ Request::METHOD_PUT })
+     * @return JsonResponse
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function putResult(?Result $result, Request $request): JsonResponse
+    {
+        if (null === $result) {
+            return $this->error(Response::HTTP_NOT_FOUND, 'NOT FOUND');
+        }
+
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $datosPeticion = $request->getContent();
+        $datos = json_decode($datosPeticion, true);
+        $userId = $datos['user'] ?? null;
+        $newResult = $datos['result'] ?? null;
+        $newTimestamp = new \DateTime('now');
+
+        // Error: falta USER
+        if (null === $userId) {
+            return $this->error(Response::HTTP_UNPROCESSABLE_ENTITY, 'Falta USER');
+        }
+
+        // Error: falta RESULT
+        if (null === $newResult) {
+            return $this->error(Response::HTTP_UNPROCESSABLE_ENTITY, 'Falta RESULT');
+        }
+
+        //Error: USER no existe
+        $userDB = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find($userId);
+        if (null === $userDB) {
+            return $this->error(Response::HTTP_NOT_FOUND, 'USER NOT FOUND');
+        }
+
+        // Modificar Result
+        $result->setResult($newResult);
+        $result->setTime($newTimestamp);
+        $result->setUser($userDB);
+
+        // Hacerla persistente
+        $em->persist($result);
+        $em->flush();
+
+        // devolver respuesta
+        return new JsonResponse($result, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route(path="/{id}", name="deleteOne", methods={ Request::METHOD_DELETE })
+     * @param Result|null $result
+     * @return JsonResponse
+     */
+    public function deleteOneResult(?Result $result): JsonResponse
+    {
+        $em = $this->getDoctrine()->getManager();
+        if($result === null) {
+            return $this->error(Response::HTTP_NOT_FOUND, 'NOT FOUND');
+        } else {
+            $em->remove($result);
+            $em->flush();
+            return new JsonResponse( null, Response::HTTP_NO_CONTENT);
+        }
+    }
+
+    /**
+     * @Route(path="", name="deleteAll", methods={ Request::METHOD_DELETE })
+     * @return JsonResponse
+     */
+    public function deleteAllResults(): JsonResponse
+    {
+        $em = $this->getDoctrine()->getManager();
+        /** @var Result[] $results */
+        $results = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+
+        foreach ($results as $result) {
+            $em->remove($result);
+            $em->flush();
+        }
+        return new JsonResponse( null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @param int $statusCode
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    private function error(int $statusCode, string $message): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'message' => [
+                    'code' => $statusCode,
+                    'message' => $message
+                ]
+            ],
+            $statusCode
+        );
+    }
+}
